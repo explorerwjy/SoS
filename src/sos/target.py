98c6d59a678edef1323f629f15f825779d6c2a85
Bo Peng <ben.bog@gmail.com>
2017-11-03 08:20:58 -0500

    Fix a sos_step bug

--- src/sos/target.py
+++ src/sos/target.py
@@ -212,7 +212,7 @@ class targets(target, Sequence):
         if len(self._targets) == 1:
             return self._targets[0].exists(mode)
         else:
-            raise ValueError('Canot test existense for group of targets {}'.format(self))
+            raise ValueError(f'Canot test existense for group of targets {self!r}')
 
     def name(self):
         if len(self._targets) == 1:
